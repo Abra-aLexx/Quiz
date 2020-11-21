b27b5686227bb6e1e3a40caa819006b26cea6c5a
AlexAbra <alexgithub@gmail.com>
2020-11-21 13:29:18 +0300

    two variables task

--- TwoVariables.java
+++ TwoVariables.java
@@ -1,5 +1,10 @@
 public class TwoVariables {
     public static void main(String[] args) {
+        /*
+        * Мне в голову пришла идея изпользовать массив стартовых параметров args
+        * он уже создан и не считается как создание новой переменной.
+        * Чтобы массив не был null, я добавил в него всего один аргумент "0"
+        * */
         int a = 5;
         int b = 3;
         args[0] = Integer.toString(a);
